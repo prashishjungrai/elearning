afc637698032968a13d74041454cf9ea8853ca98
aagyashakya <aagyashakya2024@gmail.com>
2025-01-14 23:34:38 +0545

    added hover on course card

--- app/components/Course/CourseCard.tsx
+++ app/components/Course/CourseCard.tsx
@@ -1,4 +1,3 @@
-//changed
 import Ratings from "@/app/utils/Ratings";
 import Image from "next/image";
 import Link from "next/link";
@@ -24,7 +23,7 @@ const CourseCard: FC<Props> = ({ item, isProfile }) => {
     <Link
       href={!isProfile ? `/course/${item._id}` : `course-access/${item._id}`}
     >
-      <div className="w-full min-h-[35vh] backdrop-blur border  border-[#00000015]  rounded-lg p-3 shadow-sm ">
+      <div className="w-full min-h-[35vh] backdrop-blur border border-[#00000015] rounded-lg p-3 shadow-sm transition-transform transform hover:shadow-2xl hover:-translate-y-1 hover:scale-105 hover:bg-blue-50  hover:text-blue-700">
         {/* Only render Image if thumbnailUrl exists */}
         {thumbnailUrl && (
           <Image
@@ -37,31 +36,29 @@ const CourseCard: FC<Props> = ({ item, isProfile }) => {
           />
         )}
         <br />
-        <h1 className="font-Poppins text-[16px] text-black  ">
+        <h1 className="font-Poppins text-[16px] text-black">
           {item.name}
         </h1>
         <div className="w-full flex items-center justify-between pt-2">
           <Ratings rating={item.ratings} />
           <h5
-            className={`text-black   ${
-              isProfile && "hidden 800px:inline"
-            }`}
+            className={`text-black ${isProfile && "hidden 800px:inline"}`}
           >
             {item.purchased} Students
           </h5>
         </div>
         <div className="w-full flex items-center justify-between pt-3">
           <div className="flex">
-            <h3 className="text-black  ">
+            <h3 className="text-black">
               {item.price === 0 ? "Free" : item.price + "$"}
             </h3>
-            <h5 className="pl-3 text-[14px] mt-[-5px] line-through opacity-80 text-black  ">
+            <h5 className="pl-3 text-[14px] mt-[-5px] line-through opacity-80 text-black">
               {item.estimatedPrice}$
             </h5>
           </div>
           <div className="flex items-center pb-3">
             <AiOutlineUnorderedList size={20} fill="#fff" />
-            <h5 className="pl-2 text-black  ">
+            <h5 className="pl-2 text-black">
               {item.courseData?.length} Lectures
             </h5>
           </div>
